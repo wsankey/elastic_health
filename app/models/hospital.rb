868c7f4083b6ef43c14edb685c94a3f34bd4a0a6
William Sankey <WSankey@gmail.com>
2014-11-12 08:56:52 -0500

    Attempting to highlight search terms.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -4,5 +4,18 @@ class Hospital < ActiveRecord::Base
   self.inheritance_column = nil
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search({
+      highlight: {
+        pre_tags: ['<strong>'],
+        post_tags: ['</strong>'],
+        fields: {
+          name: {},
+          complaint: {}
+        }
+      }
+    })
+  end
 end
 Hospital.import

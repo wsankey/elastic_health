ed1300911134b537dfb144b1ff7850560d1ee6ac
William Sankey <WSankey@gmail.com>
2014-11-13 23:27:34 -0500

    Updated README

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -4,18 +4,5 @@ class Hospital < ActiveRecord::Base
   self.inheritance_column = nil
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
-
-  def self.search(query)
-    __elasticsearch__.search({
-      highlight: {
-        pre_tags: ['<strong>'],
-        post_tags: ['</strong>'],
-        fields: {
-          name: {},
-          complaint: {}
-        }
-      }
-    })
-  end
 end
 Hospital.import
